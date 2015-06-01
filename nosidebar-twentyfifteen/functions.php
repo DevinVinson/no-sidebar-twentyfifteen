22167eafc1922b9e520af6032d33434708696690
Devin Vinson <devin@ran.ge>
2015-05-31 21:38:19 -0400

    Initial commit of mostly finished
    It mostly works..few quirks left for fixing the text sizing and
    coloring as it adjusts in size.

---
+++ nosidebar-twentyfifteen/functions.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Functions 
+ *
+ */
+
+
+/*
+ * Add in our own child theme style after the parent
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+}
